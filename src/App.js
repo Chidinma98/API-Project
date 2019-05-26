fc86dafca4eae28d2dbc4596a015ea935c097a8e
MaryAnn Eleanya <meleanya@gmail.com>
2019-05-26 16:10:13 -0400

    project with working api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+  this.showsInput = this.showsInput.bind(this)
+  
+  }
+
+  componentDidMount() {
+    axios.get(' https://restcountries.eu/rest/v2/all')
+    .then( res => {
+     console.log(res.data);
+    })
+  }
+
+ showsInput(e){
+  e.preventDefault()
+   console.log(this.refs.continent.value)
+ 
+ 
+  }
+
+
+
+  render() {
+  
+    return (
+      <div>
+      <form onSubmit =  {this.showsInput}>
+        <input type = 'text' name = 'location' placeholder = "Choose A Contient" ref='continent' />
+        <button type = 'submit' name = 'info'>Submit</button>
+      </form>
+    
+      
+      
+      
+      
+      </div>
+    
+    )
+  }
+
+
+
+
+
 }
 
+
+
+
 export default App;
+
+
+
+
+
